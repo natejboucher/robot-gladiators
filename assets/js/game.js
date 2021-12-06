936c0a857b2281cd40ce7f05ca9d1955e6009307
Nate Boucher <natejboucher@gmail.com>
2021-12-05 23:30:05 -0500

    updated game.js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+console.log("This log is a string");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+function fight()  {
+    window.alert("The fight has begun!");
+}
+// fight();
