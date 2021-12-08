35c4920d2cead77ce19b50a85517466dc4defafd
Nate Boucher <natejboucher@gmail.com>
2021-12-07 22:25:56 -0500

    added health reset to enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  //repeate and ececute as long as enemy robot is still alive
+  while(enemyHealth > 0)  {
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -68,9 +68,13 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
+  
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++)  {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
